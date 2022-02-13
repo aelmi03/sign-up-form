608ff3ed19462ab1471e509f0ed7c5db3186a977
Abdi-Ladif Elmi <abdiladifelmi03@gmail.com>
2022-02-13 01:25:20 -0800

    Finished the form validation and styling with the button as well, only making it not disabled if all the inputs are valid

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const allInputs = document.querySelectorAll("input");
-
+const allInputs = document.querySelectorAll("input:not(.confirmed)");
+const mainPassword = document.querySelector(".password");
+const confirmedPassword = document.querySelector(".confirmed");
+const submitButton = document.querySelector(".create-button");
 const addFormValidation = (inputElement) => {
   inputElement.addEventListener("input", validateElement);
 };
+confirmedPassword.addEventListener("input", validateConfirmed);
+function validateConfirmed() {
+  const errorText = confirmedPassword.nextElementSibling;
+  if (
+    confirmedPassword.value !== "" &&
+    mainPassword.value === confirmedPassword.value
+  ) {
+    confirmedPassword.classList.add("valid");
+    confirmedPassword.classList.remove("invalid");
+    errorText.style.opacity = "0";
+  } else {
+    confirmedPassword.classList.add("invalid");
+    confirmedPassword.classList.remove("valid");
+    errorText.style.opacity = "1";
+  }
+  buttonValid();
+}
 function validateElement(inputElement) {
   const input = inputElement.target;
   const errorText = input.nextElementSibling;
+  if (input.getAttribute("type") === "password") {
+    if (confirmedPassword.value !== "") {
+      validateConfirmed();
+    }
+  }
   if (input.validity.valid) {
     input.classList.add("valid");
     input.classList.remove("invalid");
@@ -15,6 +39,15 @@ function validateElement(inputElement) {
     input.classList.remove("valid");
     errorText.style.opacity = "1";
   }
+  buttonValid();
 }
-
+const buttonValid = () => {
+  const amountOfValidInputs = document.querySelectorAll(".valid").length;
+  console.log(amountOfValidInputs);
+  if (amountOfValidInputs === 6) {
+    submitButton.removeAttribute("disabled");
+  } else {
+    submitButton.setAttribute("disabled", "");
+  }
+};
 Array.from(allInputs).forEach((input) => addFormValidation(input));
